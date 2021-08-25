7c636314e57e516f23bbad78b6fac7676a7e453b
jcarritte1 <jcarritte1@student.rccd.edu>
2021-08-25 16:48:31 -0700

    Signed-off-by: jcarritte1 <jcarritte1@student.rccd.edu>

---
+++ Hmwk/HelloWorld/main.cpp
@@ -0,0 +1,34 @@
+/*
+    Author: Jacob Carritte
+    Date:   August 25, 2021 at 4:13 PM
+    Purpose: Hello World
+    Version:First and Last
+ */
+
+//System Libraries
+#include <iostream>    //Input/Output Library
+using namespace std;   //Library Name-space
+
+//User Libraries
+
+//Global/Universal Constants -- No Global Variables
+//Science, Math, Conversions, Higher Dimensioned constants only
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Set the Random number seed
+    
+    //Declare variables
+    
+    //Initialize variables
+    
+    //Process, map inputs to outputs
+    
+    //Display your initial conditions as well as outputs.
+    cout << "Hello World" << endl;
+    //Exit stage right
+    return 0;
+}
+
